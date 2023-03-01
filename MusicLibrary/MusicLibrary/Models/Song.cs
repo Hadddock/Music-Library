2e7d11d62c28d663768c642e72b44fa44d8d48e5
Justin Haddock <jhadddock@protonmail.com>
2023-02-28 22:00:31 -0800

    Add input validation

--- MusicLibrary/MusicLibrary/Models/Song.cs
+++ MusicLibrary/MusicLibrary/Models/Song.cs
@@ -7,12 +7,20 @@ namespace MusicLibrary.Models;
 public class Song
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Title { get; set; } = string.Empty;
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
     public string? Genre { get; set; }
+
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? Artist { get; set; }
+    [StringLength(60, MinimumLength = 3)]
     public string? Album { get; set; }
 
 }
